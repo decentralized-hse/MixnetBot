17c32e08bae0c668773e7da68821322b65e73714
Сергей Пищулов <57267495+SergeyPishchulov@users.noreply.github.com>
2022-01-16 16:44:06 +0500

    rjust

--- DesktopClient/app.py
+++ DesktopClient/app.py
@@ -55,7 +55,8 @@ class MixerMessenger:
         messages = self.app.get_chat(user)
         for m in messages:
             if m.direction == "outgoing":
-                self.chat_cell.add_item(f"                                               {m.text}")
+                self.chat_cell.add_item(m.text.rjust(20))
+                # self.chat_cell.add_item(f"                                               {m.text}")
             else:
                 self.chat_cell.add_item(m.text)
 
