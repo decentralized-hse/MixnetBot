ccd628eb918b0298c5c32b203b5c270e487df197
SergeyPishchulov <lcmrb26@bk.ru>
2022-08-18 20:23:02 +0500

    connection attempts are rejected

--- Server/server.py
+++ Server/server.py
@@ -6,6 +6,7 @@ import sys
 
 from Server.DTOs.Message import RedirectMessageDto, FinalMessageDto
 from Server.DTOs.SecretMessage import SecretMessageDto
+from Server.Types import MixerName
 
 sys.path.append('..')
 import websockets
@@ -21,82 +22,88 @@ async def get_mixers():
     # ]
 
 
-async def establish_connections_with_mixers(connections):
+async def establish_connections_with_mixers():
     ip_by_name = await get_mixers()
     for mixer_name in ip_by_name:
         if name == mixer_name:
             continue
         try:
             print(f"{xport} try connect to {mixer_name}")
-            async with websockets.connect(ip_by_name[mixer_name],
-                                          ping_interval=2) as websocket:
+            async with websockets.connect(ip_by_name[mixer_name], ping_interval=2) as websocket:
                 # TODO only not connected.  TODO fix blocking connection atempt
                 await websocket.send(jp.encode(GreetingDto(name=name)))
                 raw = await websocket.recv()
                 greet_result = jp.decode(raw)
                 if greet_result.accepted:
-                    connections[mixer_name] = websocket
+                    socket_by_name[mixer_name] = websocket
+                    try:
+                        await handle_existing_connection(websocket)
+                    finally:
+                        await unregister(mixer_name)
         except ConnectionRefusedError:
-            print(f"{xport} refused", mixer_name)
-    print(f"{xport} Established", connections)
-    if connections:
-        print(list(connections.values())[0], "*****")
-    # return connections
+            print(f"{xport} rejection", mixer_name)
+    print(f"{xport} Established", socket_by_name)
 
 
-async def send_acceptance(websocket, greeting: GreetingDto):
-    # print(f"{xport} found greet request", websocket.request_headers)
+# async def handle_message2(websocket, event: SecretMessageDto):
+#     mdto = jp.decode(event.body)
+#     # decrypted
+#     if type(mdto) is FinalMessageDto:
+#         print("FINAL")
+#         return  # save to db
+#     if type(mdto) is RedirectMessageDto:
+#         print("####")
+#         print(f"I am {xport}", mdto.to, socket_by_name)
+#         ws = socket_by_name.get(mdto.to)
+#         if not ws:
+#             raise KeyError(f"Couldn't find {mdto.to} in {list(socket_by_name.keys())}")
+#         await ws.send(mdto.body)
+#         print("resent")
+#         return
+#     raise RuntimeError(f"Couldn't resend a message {mdto}")
 
-    connections[greeting.name] = websocket
+
+async def register(websocket) -> MixerName:
+    raw = await websocket.recv()
+    greeting = jp.decode(raw)
+    if type(greeting) is not GreetingDto:
+        raise ConnectionRefusedError("Wrong Greeting message")
+    socket_by_name[greeting.name] = websocket
     await websocket.send(jp.encode(GreetingResultDto(accepted=True)))
+    return greeting.name
 
 
-async def handle_message(websocket, event: SecretMessageDto):
-    mdto = jp.decode(event.body)
-    # decrypted
-    if type(mdto) is FinalMessageDto:
-        print("FINAL")
-        return  # save to db
-    if type(mdto) is RedirectMessageDto:
-        print("####")
-        print(f"I am {xport}", mdto.to, connections)
-        ws = connections.get(mdto.to)
-        if not ws:
-            raise KeyError(f"Couldn't find {mdto.to} in {list(connections.keys())}")
-        await ws.send(mdto.body)
-        print("resent")
-        return
-    raise RuntimeError(f"Couldn't resend a message {mdto}")
+async def unregister(mixer_name: MixerName):  # TODO delete async?
+    del socket_by_name[mixer_name]
 
 
-async def router(websocket):
-    message = await websocket.recv()
-    event = jp.decode(message)
-    if type(event) is GreetingDto:
-        # print(f"{xport} GDto {websocket.response_headers['Host']}")
-        await send_acceptance(websocket, event)
-        print(f"{xport} Now cons:", connections)
-    if type(event) is SecretMessageDto:
-        print("received secret message", event)
-        await handle_message(websocket, event)
-    #     if "join" in event:
-    #         # Second player joins an existing game.
-    #         await join(websocket, event["join"])
-    #     elif "watch" in event:
-    #         # Spectator watches an existing game.
-    #         await watch(websocket, event["watch"])
-    #     else:
-    #         # First player starts a new game.
-    #         await start(websocket)
-    # async for message in websocket:
-    #     print("received form client" + message)
-    #     await websocket.send("Hi from server")
+async def handle_existing_connection(websocket):
+    async for raw_message in websocket:
+        message = jp.decode(raw_message)
+        if type(message) is SecretMessageDto:
+            print("received secret message", message)
+
+
+async def handler(websocket):
+    mixer_name = await register(websocket)
+    try:
+        await handle_existing_connection(websocket)
+    finally:
+        await unregister(mixer_name)
+
+
+async def background_update():
+    while True:
+        await establish_connections_with_mixers()
+        await asyncio.sleep(1)
 
 
 async def main(xport):
-    await establish_connections_with_mixers(connections)  # TODO explicit return connections
+    # task = asyncio.create_task(establish_connections_with_mixers())
+    task = asyncio.create_task(background_update())
+    await task  # не должен блокировать # TODO explicit return connections
 
-    async with websockets.serve(router, "localhost", xport):
+    async with websockets.serve(handler, "localhost", xport):
         await asyncio.Future()  # run forever
 
 
@@ -106,5 +113,5 @@ if __name__ == '__main__':
     args = parser.parse_args()
     xport = args.xport
     name = str(xport)
-    connections = {}
+    socket_by_name = {}
     asyncio.run(main(xport))
